55b288f0fc8af32f867e06160a8011830d05ee60
kiran76 <ksknew76@gmail.com>
2024-05-16 01:28:45 +0530

    created comments section

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,8 +2,8 @@ import { Image } from "./Image"
 
 export const Comment = ({name, comment}) =>{
     return(
-        <div className="comment flex gap-6 items-center">
-            <Image/>
+        <div className="comment flex gap-6 items-center mt-4 mb-3">
+            <Image width="w-[10%]"/>
             <div>
                 <h2 className="text-green-600">{name}</h2>
                 <p className="text-gray-300">{comment}</p>
--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,9 +1,29 @@
+import { useState } from "react";
+import { border } from "../utils/class";
+import { Comment } from "./Comment";
+import { Image } from "./Image";
 import { Text } from "./Italic";
+import { IoMdSend } from "react-icons/io";
 
 const Comments = () =>{
+    const [input, setInput] = useState("");
     return(
         <div className="m-6">
             <Text text="Comments" weight={"font-bold"} icon={""}/>
+            <Comment name={"Jane"} comment={"Thanks for assigning"}/>
+            <div className="send flex gap-4 items-center m-2">
+                <Image width="w-[10%]"/>
+                <div className={`flex items-center rounded-[3em] ${border}`}>
+                    <input className={`p-4 text-gray-300 `} placeholder="Write a comment..."
+                    value={input}
+                    onChange={(e)=>{
+                        setInput(e.target.value)
+                    }}
+                    />
+
+                    <div className="m-2 cursor-pointer"><IoMdSend/></div>
+                </div>
+            </div>
         </div>
     )
 

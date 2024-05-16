8a094c3c7eafe26ed26bd79381ec81fb5abc778b
kiran76 <ksknew76@gmail.com>
2024-05-16 18:51:51 +0530

    dropdown functionality changed

--- src/components/Assign.js
+++ src/components/Assign.js
@@ -5,42 +5,55 @@ import { Image } from "./Image";
 import { Assignes } from "../utils/class";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
+import { useState } from "react";
 
 
-const Assign = () =>{
+const Assign = () => {
     const dispatch = useDispatch();
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [selectedUser, setSelectedUser] = useState(Assignes[0]); 
 
     const handleUserChange = (item) => {
-        console.log(item)
+        console.log(item);
         dispatch(addUser(item));
-        
-      };
-    // const user = useSelector(store => store.user.users)
-    return(
+        setSelectedUser(item);
+        setIsDropdownOpen(false); 
+    };
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
+    return (
         <div className="m-7 items-center flex justify-between">
-            <Text text="Assign to:" icon={<CiUser/>}/>
+            <Text text="Assign to:" icon={<CiUser />} />
             
             <div className="rounded-[3rem] border-gray-300 border-l border-r border-t border-b p-2 flex items-center w-[12em] text-green-400">
-                <Image/>
-                <select className="mx-4" onChange={(e)=>{
-                    handleUserChange(e.target.value)
-                }}>
-                    {Assignes.map((item, index)=>(
-                        <option key={index} value={item} >{item}</option>
-                    ))}
-           
-                    {/* <option value="fruit">Fruit</option>
-
-                    <option value="vegetable">Vegetable</option>
-
-                    <option value="meat">Meat</option> */}
-                </select>
-                <div className="cursor-pointer"><IoMdArrowDropdown/></div>
+                <Image />
+                <div className="relative w-full">
+                    <div className="cursor-pointer flex justify-between items-center" onClick={toggleDropdown}>
+                        <span>{selectedUser}</span>
+                        <IoMdArrowDropdown />
+                    </div>
+                    {isDropdownOpen && (
+                        <select
+                            className="absolute top-full left-0 w-full mt-2 bg-white border border-gray-300 rounded-md"
+                            value={selectedUser}
+                            onChange={(e) => handleUserChange(e.target.value)}
+                            onBlur={() => setIsDropdownOpen(false)}
+                        >
+                            {Assignes.map((item, index) => (
+                                <option key={index} value={item}>
+                                    {item}
+                                </option>
+                            ))}
+                        </select>
+                    )}
+                </div>
             </div>
-
-
         </div>
+    );
+};
 
-    )
-}
+
 export default Assign;

1d1610b9da710f499e9189ee1dd3d8f281682f7c
kiran76 <ksknew76@gmail.com>
2024-05-16 18:31:51 +0530

    added event marking functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,16 @@
 import { CiCircleCheck } from "react-icons/ci";
 import { AiOutlineDelete } from "react-icons/ai";
 import { IoMdClose } from "react-icons/io";
+import { useState } from "react";
+import { MdIncompleteCircle } from "react-icons/md";
 
 const NavBar = () =>{
+    const [event, setmarkEvent] = useState(false)
+
     return(
         <div className="flex justify-between">
-            <div className="m-4 text-2xl text-red-600 cursor-pointer font-bold"><CiCircleCheck/></div>
+            
+            <div onClick={()=>setmarkEvent(true)} className="m-4 text-2xl text-red-600 cursor-pointer font-bold">{event ? <CiCircleCheck/> : <MdIncompleteCircle/>}</div>
 
             <div className="flex gap-1">
                 <div className="m-4 text-2xl text-red-600 cursor-pointer font-bold"><AiOutlineDelete/></div>
